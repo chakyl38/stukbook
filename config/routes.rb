7cc678406971e731a52940d555e338a7d45f8b84
Kyle Chapman <kylechapman09@gmail.com>
2015-07-19 15:32:47 -0500

    Friendships actions now possible

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   resources :users, only: [:index, :show]
+  resources :friendships, only: [:create, :destroy, :accept] do 
+    member do 
+      put :accept
+    end
+  end
 end
