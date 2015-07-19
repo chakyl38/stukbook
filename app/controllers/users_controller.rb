95c224fe657b409f09be9c3e19b30fc7f04b8400
Kyle Chapman <kylechapman09@gmail.com>
2015-07-19 17:46:47 -0500

    Added posts controller, views, and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    # @post = Post.new
-    # @posts = @user.posts.order('created_at DESC')
+    @post = Post.new
+    @posts = @user.posts.order('created_at DESC')
     @activities = PublicActivity::Activity.where(owner_id: @user.id).order('created_at DESC')
   end
 
